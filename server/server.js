d07294ce1338b8ede09acc268fda9ddbd1ac233e
TEKami13 <tobias.krogh@hotmail.com>
2023-09-19 21:53:34 +0200

    Edited the files according to the "Implement GET /api/questions/random" commit by Johannes

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 import express from "express";
+import {randomQuestion} from "./questions.js";
 
 const app = express();
 
 app.use(express.static("../client/dist"));
 
+app.get("/api/question/random", (req, res) => {
+    res.json(randomQuestion()).send()
+})
 
 app.listen(3000)
