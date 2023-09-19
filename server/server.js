f46dc875504433101bda0e7d31117fa9fcd47bf0
TEKami13 <tobias.krogh@hotmail.com>
2023-09-19 22:37:04 +0200

    Edited the files according to the "Implement scoring in signed cookie" commit by Johannes

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import bodyParser from "body-parser";
+import cookieParse from "cookie-parser";
 import {isCorrectAnswer, randomQuestion, Questions} from "./questions.js";
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cookieParse("random-noise-to-sign-cookies"));
 app.use(express.static("../client/dist"));
 
 app.get("/api/question/random", (req, res) => {
@@ -15,11 +17,27 @@ app.post("/api/questions/answer", (req, res) => {
     const {id, answer} = req.body;
     const question = Questions.find(q => q.id === id);
     const correct = isCorrectAnswer(question, answer);
-    res.json({correct})
+
+    const score = req.signedCookies?.quizScore
+        ? JSON.parse(req.signedCookies.quizScore)
+        : {answers: 0, correctAnswer: 0}
+
+    const {answers, correctAnswers} = score;
+    const newScore = {
+        answers: answers+1,
+        correctAnswers: correctAnswers + (corrext ? 1 : 0)
+    }
+    console.log(req.signedCookies, {score, newScore});
+    res
+        .cookies("quizScore", JSON.stringify(newScore), {signed: true})
+        .json({correct})
 })
 
 app.get("/api/score", (req, res) => {
-    const score = 0;
+    const score = req.signedCookies?.quizScore
+        ? JSON.parse(req.signedCookies?.quizScore)
+        : {answers: 0, correctAnswers: 0}
+    res.json(score);
     res.json({score});
 })
 
